aa754a31e135ac7114eb6bf0f0fe787254e4f8b4
xiaojieluo <xiaojieluoff@gmail.com>
2018-02-25 08:15:06 +0800

    编写 config
    Signed-off-by: xiaojieluo <xiaojieluoff@gmail.com>

--- calla/config.py
+++ calla/config.py
@@ -1,177 +1,84 @@
 import sys, os
 import re
 from redbaron import RedBaron
-from pelican.settings import DEFAULT_CONFIG, get_settings_from_file, get_settings_from_module
 from unipath import Path
 import tempfile
 import importlib
 import pprint
 import json
+import toml
+from calla.model import Setting
+import types
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-class BaseConfig(object):
-    pass
-
-class PelicanConfig(object):
-    pass
-
-class DefaultConfig(object):
-    pass
-
-# def get_settings_from_file(path, default_settings={}):
-#     """Loads settings from a file path, returning a dict."""
-#
-#     name, ext = os.path.splitext(os.path.basename(path))
-#     module = load_source(name, path)
-#     return get_settings_from_module(module, default_settings=default_settings)
-
-'''
-DEFAULT_PELICAN_SETTINGS = {
-    'SERVER_DEBUG': True
-}
-'''
-
-
-def load_source(name, path):
-    loader = importlib.machinery.SourceFileLoader(name, path)
-    spec = importlib.util.spec_from_loader(loader.name, loader)
-    mod = importlib.util.module_from_spec(spec)
-    loader.exec_module(mod)
-    return mod
-
-# def get_settings_from_file(path, default_settings = None):
-#     if default_settings is None:
-#         default_settings = {}
-#     name, ext = os.path.splitext(os.path.basename(path))
-#     module = load_source(name, path)
-#     return get_settings_from_module(module, default_settings = default_settings)
-
-class Config(object):
-    ''' 代理类'''
+class Config(dict):
+    ''' config 类
+    attribute only read , cannot modified
+    '''
     config_file = None
 
-    def __init__(self, path = None):
-        if path is None:
-            path = os.path.join(os.getcwd(), 'pelicanconf.py')
-        if self.config_file:
-            path = self.config_file
-        self._path = path
-        self._pelicanconf = get_settings_from_file(self._path)
-        self._baron = None
-
-    def set(self, key, value):
-        self.__dict__[key] = value
-
     def get(self, key, default = None):
-        '''从 pelicanconf.py 中取出配置
-        Args:
-            key: 要取出的变量名
-            default: 配置不存在时的默认值
-        '''
-        key = key.upper()
-        return self._pelicanconf.get(key, default)
-
+        if key in self:
+            data = self[key]
+            # 当 self[key] 是字典的时候， 返回本类实例
+            if isinstance(data, dict):
+                data = self.__class__(data)
+            return data
+        return default
 
     def __getattr__(self, key):
         return self.get(key)
 
+    def set(self, key, value):
+        self.update({key: value})
+
     def __setattr__(self, key, value):
         self.set(key, value)
 
-    def __getitem__(self, key):
-        return self.get(key)
-
-
-    def update(self, key, value):
-        '''更新配置
-        '''
-        return self.set(key, value)
-
-    def delete(self, key):
-        ''' 删除 key 配置变量'''
-        index = self.find_baron(key.upper())
-        if index:
-            del self.baron[index]
-            print(self.baron)
-            self.save()
-
-    def find_baron(self, key):
-        ''' 从 redbaron 中提取配置'''
-        not_found = True
-        index = None
-        red = self.baron
-        for i in range(0, len(red)):
-            try:
-                target = red[i].target.value
-            except:
-                target = ''
-            if target == key:
-                not_found = False
-                index = i
-
-        return index
-
-    @property
-    def baron(self):
-        if not self._baron:
-            with open(self._path, 'r') as fp:
-                text = fp.read()
-            red = RedBaron(text)
-            self._baron = red
-
-        return self._baron
-
     def save(self):
         ''' 保存配置到 self._path '''
-        keys = [k for k in self.__dict__.keys() if k[0] != '_']
-        red = self.baron
-        for key in keys:
-            up_key = key.upper()
-            not_found = True
-            for i in range(0, len(red)):
-                try:
-                    target = red[i].target.value
-                except:
-                    target = ''
-                if target == up_key:
-                    not_found = False
-                    value = self.__dict__[key]
-                    if isinstance(value, str):
-                        value = '"{}"'.format(value)
-                    elif isinstance(value, dict):
-                        # value = "{}".format(value)
-                        print("DICT")
-                        # multi = json.loads(value)
-                        value = json.dumps(value, indent=4)
-                        print(value)
-                        # value = dumps(value)
-                    else:
-                        value = "{}".format(value)
-                    red[i].value = value
-
-            if not_found:
-                # 不存在， 在末尾新增
-                format_ = '{key} = "{value}"'.format(key = key.upper(), value = self.__dict__[key])
-                red.append(format_)
-
-
         with open(self._path, 'w') as fp:
-            text = red.dumps()
-            fp.write(text)
+            toml.dump(self, fp)
+        return True
 
-        # 清除缓存
-        self._baron = None
+    def delete(self, key):
+        '''
+        删除配置
+        Args:
+            key: 要删除的 key
+        '''
+        if key in self:
+            del self[key]
 
+    @classmethod
+    def load(cls, path):
+        ''' 静态变量， 从 toml 文件中加载配置并注入 Config 类中。 '''
+        config = toml.load(path, cls)
+        instance = cls()
+        instance.update(config)
+        instance.__dict__['_path'] = path
+        return instance
 
     @classmethod
     def monkey_patch(cls, path):
         cls.config_file = path
 
-def make_config():
-    ''' 根据配置文件组装 config '''
-    _config = Config()
+    def __delattr__(self, key):
+        return self.__delitem__(key)
+
+    def __delitem__(self, key):
+        if key in self:
+            self.pop(key)
+
+def make_config(path = None):
+    ''' 根据配置文件组装 config
+    并且用用户自定义配置覆盖默认配置
+    '''
+    if path is None:
+        path = os.path.join(os.getcwd(), 'calla.toml')
+    _config = Config.load(path)
     config = _config
-    workname = os.path.dirname(config._path)
-    config.path = os.path.join(workname, config.path)
+    # workname = os.path.dirname(config._path)
+    # config.path = os.path.join(workname, config.path)
     return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,21 +1,52 @@
 import pytest
-from pelican_manager.config import Config
-import sys
+from calla.config import make_config, Config
+import sys, os
+import toml
 
 @pytest.fixture()
-def config(request):
-    config  = Config('tests/pelicanconf.py')
+def path(request):
+    path = os.path.join(os.getcwd(), 'tests/calla.toml')
+    return path
+
+@pytest.fixture()
+def config(request, path):
+    config = make_config(path)
     return config
 
-def test_config_get(config):
-    assert config.SITEURL == 'Test'
-    assert isinstance(config.STATIC_PATHS, list)
+def test_config_delete(config):
+    del config['date_format']['en_us']
+    assert 'en_us' not in config['date_format']
+    del config['date_format']['not exists']
 
-def test_config_set(config):
-    config.default_lang = 'zh'
+def test_config_save(config):
+    config['date_format']['test_time'] = '%Y'
+    config.save()
+    reconf = make_config(config._path)
+    assert 'test_time' in reconf['date_format']
+
+    #clean
+    del config['date_format']['test_time']
     config.save()
 
-    # back
-    assert config.default_lang == 'zh'
-    config.default_lang = 'en'
-    config.save()
+def test_config_itemattr(config):
+    # get
+    assert config['title'] is not None
+    assert config['date_format'].zh_cn is not None
+    # set
+    config['date_format']['test_config_itemattr'] = True
+    assert config['date_format']['test_config_itemattr'] is True
+
+def test_config_attribute(config):
+    # only get, cannot set attribute
+    assert config.title is not None
+    assert config.date_format.zh_cn is not None
+
+def test_config_get_method(config):
+    assert config.get('title') is not None
+    assert config.get('date_format').get('zh_cn') is not None
+    assert config.get("not exists", 'Unknow')  == 'Unknow'
+
+
+def test_config_set_method(config):
+    config.test = 'hello'
+    assert config.test == 'hello'
