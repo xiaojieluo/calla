a826605793240a82bd140274a105ab35b585d8ab
xiaojieluo <xiaojieluoff@gmail.com>
2018-03-05 17:55:57 +0800

    修改单元测试
    Signed-off-by: xiaojieluo <xiaojieluoff@gmail.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,12 @@
 import pytest
-from pelican_manager.config import Config
-from pelican_manager import make_app
+from calla.config import Config
+from calla import make_app
 import json
-from pelican_manager.utils import iterdir
+from calla.utils import iterdir
 
 @pytest.fixture()
 def client(request):
-    app = make_app('tests/pelicanconf.py')
+    app = make_app('tests/calla.toml')
     app.config['TESTING'] = True
     client = app.test_client()
 
@@ -27,15 +27,3 @@ def test_get_all_articles_lists_GET(client):
     assert 'total' in data
     assert isinstance(data['total'], int)
     assert isinstance(data['results'], list)
-
-
-PATHS = [path for path in iterdir('tests/content')]
-
-@pytest.fixture(params = PATHS)
-def path(request):
-    return request.param
-
-def test_get_one_article_info_GET(client, path):
-    url = '/api/articles/{}'.format(path)
-    res = client.get(url)
-    assert res.status_code == 200
